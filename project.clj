15705229cf01ef2919a3c12b0531ae87583bc487
paomian <tanglnan1994@gmail.com>
2013-08-13 20:47:43 +0800

    modified:   project.clj 	modified:   src/hello_world/view/index.clj 	resources/public/bootstrap/

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject hello_world "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A test for clojure learning"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
   				 [hiccup "1.0.4"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.5"]
+                 [hickory "0.5.0"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler hello_world.handler/app}
   :profiles
--- src/hello_world/view/index.clj
+++ src/hello_world/view/index.clj
@@ -1,10 +1,40 @@
 (ns hello_world.view.index
-	(:use [hiccup core page]))
+	(:use [hiccup core page]
+		  [hiccup.form]))
 
 (defn index-page []
 	(html5
 		[:head
-		   [:title "Hello World"]
-		   (include-css "/css/mycss.css")]
-	    [:body
-	    	[:h1 [:center "Hello World!"]]]))
+		  [:title "Hello World"]
+		  (include-css "bootstrap/css/bootstrap.min.css")
+		  (include-css "bootstrap/css/bootstrap-responsive.min.css")
+		  (include-js "bootstrap/js/jquery-2.0.2.min.js")
+		  (include-js "bootstrap/js/bootstrap.min.js")
+		  ]
+		[:body
+		    [:div.navbar.navbar-inverse 
+		      [:div.navbar-inner 
+		        [:div.container 
+		          [:a.btn.btn-navbar {:data-toggle "collapse" :data-target ".nav-collapse"}
+		            (for [x (range 4)]
+		            	[:span.icon-bar])]
+		          [:a.brand {:href "#"} "Porject name"]
+		          [:ul.nav 
+		          	[:li [:a {:href "#"} "Home"]]
+		          	[:li [:a {:href "#"} "Link"]]
+		          	[:li [:a {:href "#"} "some"]]
+		          	]
+		          [:form.navbar-search.pull-left [:input.search-query.span2 {:type "text" :placeholder "Search"}]]
+		          [:div.nav-collapse.collapse]]]]
+		    [:div.container 
+		      (form-to [:post "/test"]
+		      	[:table
+		      	  [:tr
+		      	    [:td (label :user "Username")]
+		      	    [:td (text-field :user)]]
+		      	  [:tr
+		      	    [:td (label :pwd "Password")]
+		      	    [:td (password-field  :pwd)]]
+		      	  [:tr
+		      	    [:td]
+		      	    [:td [:button.btn.btn-primary {:type "submit"} "Log In"]]]])]]))
