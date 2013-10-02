5c0dfb29d1d0f49a73bea47e31fa716b0291003e
paomian <tanglnan1994@gmail.com>
2013-10-02 21:28:39 +0800

    modified:   src/hello_world/index.clj 	resources/

--- src/hello_world/index.clj
+++ src/hello_world/index.clj
@@ -10,6 +10,7 @@
            [:div.jumbotron 
             [:h1 "一个还没有投入运营的时时聊天的平台，完全运用web服务，只要一个可以用的浏览浏览器"]
             [:p.lead "其实也没啥，现在就尼玛有个登陆注册，其他的什么都没有"]
+            [:p.lead "终于在2013年的某一天有了聊天功能。。。"]
             (let [user (session/get :user)]
               (if user 
                 [:a.btn.btn-large.btn-danger {:href "/chat"} "聊天"]
